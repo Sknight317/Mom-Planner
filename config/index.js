5751f52c181ef84614d37bef4b8c2dc230477d25
Sknight317 <Sknight23@mail.valenciacollege.edu>
2019-01-27 18:41:30 -0500

    initial

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const dbuser = 'Sknight317';
+const dbpassword = 'srknigh2';
+
+const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds213255.mlab.com:13255/heroku_rb1vl1g7`;
+
+module.exports = MONGODB_URI;
