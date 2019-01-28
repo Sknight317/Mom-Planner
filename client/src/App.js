9a0d5066ddea35b99762b8798a5f1f5832cf6f8f
Sknight317 <Sknight23@mail.valenciacollege.edu>
2019-01-28 04:11:07 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
        <Navbar />
-      <Landing />
+       <Route exact path="/" component={Landing} />
+       <Route exact path="/register" component={Register} />
+       <Route exact path="/login" component={Login} />
       </div>
+      </Router>
     );
   }
 }
